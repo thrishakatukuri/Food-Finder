98d9573f528cfc0f43206b024048d3f2c3f08c6d
thrishakatukuri <katukurithrisha12@gmail.com>
2025-07-23 20:05:05 +0530

    outputs

---
+++ src/assets/APIs.jsx
@@ -0,0 +1,8 @@
+// API’s & Website-links
+// 1. ALL API LIST:        https://www.themealdb.com/api.php
+// 2. Meal Categories:     https://www.themealdb.com/api/json/v1/1/categories.php
+// 3. Search meal by name: http://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}
+// 4. Meal details by id:  https://www.themealdb.com/api/json/v1/1/lookup.php?i={ID}
+// 5. Filter by Category:  www.themealdb.com/api/json/v1/1/filter.php?c= ${Category}
+// 6.Git-hub:              https://github.com/thrishakatukuri/Food-Finder.git 
+// 7.Out-puts:             https://docs.google.com/document/d/1uWb8p1yQjB5dCO5dJSmITFKIW1MPlQNY/edit?usp=sharing&ouid=105562276394918191114&rtpof=true&sd=true
